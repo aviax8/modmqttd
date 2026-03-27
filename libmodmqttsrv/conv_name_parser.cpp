d9b9cb74592a092fcd4882f75ac18692cc7c4e28
Richard Pecl <aviax@aviax.cz>
2026-03-27 22:02:12 +0100

    Plugin evaluate() arguments parser allows complex expressions with =, comma and quotes  inside a quoted string

--- libmodmqttsrv/conv_name_parser.cpp
+++ libmodmqttsrv/conv_name_parser.cpp
@@ -1,9 +1,11 @@
 #include "conv_name_parser.hpp"
 
+#include <cctype>
 #include <exception>
 #include <memory>
 #include <regex>
 #include <string>
+#include <string_view>
 
 #include "libmodmqttconv/convargs.hpp"
 
@@ -80,122 +82,144 @@ ConverterArgParser::parse(const std::string& argStr) {
     ConverterArgValues ret(mArgs);
 
     mProcessedArgs.clear();
+    mUseArgOrder = true;
     mCurrentPosArgIterator = mArgs.begin();
-    mCurrentState.push(SCAN);
+    mArgName.clear();
+    mArgValue.clear();
 
-    char str_delimiter = 0x0;
+    auto trimView = [](std::string_view v) -> std::string_view {
+        while (!v.empty() && std::isspace(static_cast<unsigned char>(v.front())))
+            v.remove_prefix(1);
+        while (!v.empty() && std::isspace(static_cast<unsigned char>(v.back())))
+            v.remove_suffix(1);
+        return v;
+    };
 
-    for(const char& c: argStr) {
-        switch(c) {
-            case '\\':
-                switch(mCurrentState.top()) {
-                    case SCAN: mCurrentState.push(ESCAPE); break;
-                    case ARGVALUE: mArgValue += c; break;
-                    case ESCAPE:
-                        mArgValue += getEscapedChar(c);
-                        mCurrentState.pop();
-                    break;
-                }
-            break;
-            case '=':
-                switch(mCurrentState.top()) {
-                    case SCAN:
-                        if (mArgValue.empty())
-                            throw ConvNameParserException("Missing name for argument " + std::to_string(ret.count() + 1));
-                        if (!mArgName.empty())
-                            throw ConvNameParserException("Name for argument " + std::to_string(ret.count() + 1) + " already set to " + mArgName);
-                        mArgName = validateArgName(mArgValue);
-                        mArgValue.clear();
-                    break;
-                    case ARGVALUE:
-                        throw ConvNameParserException("Name for argument " + std::to_string(ret.count() + 1) + " cannot be quoted");
-                    break;
-                    case ESCAPE:
-                        mArgValue += getEscapedChar(c);
-                        mCurrentState.pop();
-                    break;
-                }
-            break;
-            case ',':
-                switch(mCurrentState.top()) {
-                    case SCAN:
-                        if (mArgValue.empty())
-                            throw ConvNameParserException("Argument " + std::to_string(ret.count() + 1) + " is empty");
-                        setArgValue(ret);
-                    break;
-                    case ARGVALUE: mArgValue += c; break;
-                    case ESCAPE:
-                        mArgValue += getEscapedChar(c);
-                        mCurrentState.pop();
-                    break;
-                }
-            break;
-            case '"':
-                switch(mCurrentState.top()) {
-                    case SCAN:
-                        mCurrentState.push(ARGVALUE);
-                        str_delimiter = c;
-                    break;
-                    case ARGVALUE:
-                        if (str_delimiter == c)
-                            mCurrentState.pop();
-                        else
-                            mArgValue += c;
-                    break;
-                    case ESCAPE:
-                        mArgValue += getEscapedChar(c);
-                        mCurrentState.pop();
-                    break;
-                }
-            break;
-            case '\'':
-                switch(mCurrentState.top()) {
-                    case SCAN:
-                        mCurrentState.push(ARGVALUE);
-                        str_delimiter = c;
-                    break;
-                    case ARGVALUE:
-                        if (str_delimiter == c)
-                            mCurrentState.pop();
-                        else
-                            mArgValue += c;
-                        break;
-                    case ESCAPE:
-                        mArgValue += getEscapedChar(c);
-                        mCurrentState.pop();
-                    break;
-                }
-            break;
-            case ' ':
-                switch(mCurrentState.top()) {
-                    case SCAN: break;
-                    case ARGVALUE: mArgValue += c; break;
-                    case ESCAPE: mArgValue += getEscapedChar(c); mCurrentState.pop(); break;
-                }
-            break;
-            default:
-                switch(mCurrentState.top()) {
-                    case SCAN:
-                        mArgValue += c;
-                        break;
-                    case ARGVALUE: mArgValue += c; break;
-                    case ESCAPE: mArgValue += getEscapedChar(c); mCurrentState.pop(); break;
-                }
+    auto isQuoted = [](std::string_view v) -> bool {
+        return v.size() >= 2 &&
+               ((v.front() == '\'' && v.back() == '\'') ||
+                (v.front() == '"'  && v.back() == '"'));
+    };
 
-            break;
-        };
+    auto unquote = [isQuoted](std::string_view v) -> std::string {
+        if (isQuoted(v))
+            v = v.substr(1, v.size() - 2);
+        return std::string(v);
+    };
+
+    auto findTopLevelEqual = [](std::string_view v) -> std::size_t {
+        char quote = '\0';
+        bool escape = false;
+
+        for (std::size_t i = 0; i < v.size(); ++i) {
+            const char c = v[i];
+
+            if (escape) {
+                escape = false;
+                continue;
+            }
+
+            if (quote != '\0') {
+                if (c == '\\') {
+                    escape = true;
+                } else if (c == quote) {
+                    quote = '\0';
+                }
+                continue;
+            }
+
+            if (c == '\'' || c == '"') {
+                quote = c;
+                continue;
+            }
+
+            if (c == '=') {
+                return i;
+            }
+        }
+
+        return std::string_view::npos;
+    };
+
+    auto flushToken = [&](std::string_view rawToken) {
+        rawToken = trimView(rawToken);
+
+        if (rawToken.empty()) {
+            throw ConvNameParserException("Argument " + std::to_string(ret.count() + 1) + " is empty");
+        }
+
+        const std::size_t eqPos = findTopLevelEqual(rawToken);
+
+        mArgName.clear();
+        mArgValue.clear();
+
+        if (eqPos == std::string_view::npos) {
+            // Positional argument; quoted text stays a single value.
+            mArgValue = unquote(trimView(rawToken));
+        } else {
+            auto nameView = trimView(rawToken.substr(0, eqPos));
+            auto valueView = trimView(rawToken.substr(eqPos + 1));
+
+            if (nameView.empty())
+                throw ConvNameParserException("Missing name for argument " + std::to_string(ret.count() + 1));
+            if (valueView.empty())
+                throw ConvNameParserException("Missing value for argument " + std::to_string(ret.count() + 1));
+
+            if (isQuoted(nameView))
+                throw ConvNameParserException(
+                    "Name for argument " + std::to_string(ret.count() + 1) + " cannot be quoted");
+
+            mArgName = validateArgName(std::string(nameView));
+            mArgValue = unquote(valueView);
+        }
+
+        setArgValue(ret);
+    };
+
+    std::string_view input(argStr);
+
+    // empty argument list is valid, e.g. std.uint8()
+    if (trimView(input).empty()) {
+        return ret;
     }
 
-    switch(mCurrentState.top()) {
-        case SCAN:
-            if (mArgValue.size())
-                setArgValue(ret);
-            break;
-        case ARGVALUE:
-            throw ConvNameParserException("Argument "s + std::to_string(ret.count()) + " is an unterminated string");
-        case ESCAPE:
-            throw ConvNameParserException("Argument "s + std::to_string(ret.count()) + " has an invalid escape sequence");
+    std::size_t tokenStart = 0;
+    char quote = '\0';
+    bool escape = false;
+
+    for (std::size_t i = 0; i < input.size(); ++i) {
+        const char c = input[i];
+
+        if (escape) {
+            escape = false;
+            continue;
+        }
+
+        if (quote != '\0') {
+            if (c == '\\') {
+                escape = true;
+            } else if (c == quote) {
+                quote = '\0';
+            }
+            continue;
+        }
+
+        if (c == '\'' || c == '"') {
+            quote = c;
+            continue;
+        }
+
+        if (c == ',') {
+            flushToken(input.substr(tokenStart, i - tokenStart));
+            tokenStart = i + 1;
+        }
     }
+
+    if (quote != '\0')
+        throw ConvNameParserException(
+            "Argument " + std::to_string(ret.count() + 1) + " is an unterminated string");
+
+    flushToken(input.substr(tokenStart));
     return ret;
 }
 
